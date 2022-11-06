25c347a84096df9759f0fb29cbe2dc75391d22d5
Ashish R <ashishcw@gmail.com>
2022-11-06 14:17:33 +0530

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+import { Router, RouterModule, ActivatedRoute, ParamMap} from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'loginPage', component: LoginPageComponent},
+      {path: '**', component: LoginPageComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
